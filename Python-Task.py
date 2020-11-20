f8512c2b0b680f1e184551487182a464e4e3f8bb
murad48oguz <74778759+murad48oguz@users.noreply.github.com>
2020-11-20 23:09:41 +0400

    Add files via upload

---
+++ Python-Task.py
@@ -0,0 +1,31 @@
+class  texnostore:
+    pass
+
+print("Texnostore magazasinin online sehifesine xos gelmisiniz. :")
+kat1=texnostore()
+kat1.ad="1. Telefon"
+print(kat1.ad)
+kat2=texnostore()
+kat2.ad="2. Komputer"
+print(kat2.ad)
+kat3=texnostore()
+kat3.ad="3. Meiset texnologiyasi"
+print(kat3.ad)
+def siyahi():
+      tellist=["Samsung", "Iphone", "Nokia", "Sony", "Alcaltel", "Xiomi"]
+      komplist=["Acer", "Apple" , "Asus", "Hp", "Casper","Del"]
+      meisetlist=["Soyuducu", " Televizor", " Tozsoran"]
+      if a== "telefon":
+          print(tellist)
+      elif a== "komputer":
+          print(komplist)
+      elif a== "meiset":
+          print(meisetlist)
+          
+          
+         
+a=input("Siyahisini gormek istediyiniz katoqoriyani qeyd edin - ").capitalize()
+
+siyahi()
+      
+      
